39e34cceb85e78a8bacec08fbfee5e74ef8d31cb
raiden_va <raiden226@list.ru>
2023-01-12 10:10:42 +0800

    feat: add html and styles

--- src/module.js
+++ src/module.js
@@ -1,17 +1,2 @@
-function sleep(time = 1000) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, time)
-  })
-}
-
-async function start() {
-  console.log('first')
-  await sleep(4000)
-
-
-  console.log('second')
-}
-
-start()
+console.log('module')
+console.log(11233222)
